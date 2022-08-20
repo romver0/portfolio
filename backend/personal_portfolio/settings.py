dbcb4dbb53def7f4c601fc2d12580270477b5be4
Romver <roma_zverev2002@mail.ru>
2022-08-20 21:20:44 +0300

    Прозошли изменения

--- backend/personal_portfolio/settings.py
+++ backend/personal_portfolio/settings.py
@@ -25,7 +25,7 @@ env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost','127.0.0.1']
 ALLOWED_HOSTS = ['*']
